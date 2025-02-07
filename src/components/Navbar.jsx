7b070a32d9c9a283494ba163058f69f799fd91ad
Bryan Li <76718507+Zy8712@users.noreply.github.com>
2025-02-07 10:08:31 -0500

    Starting of bug fixes & QoL updates.
    Specifics:
    - long overdue bug fixes being implemented
    - bug involving navbar transition from light mode to dark mode being slower than rest of page (partially resolved)
    - added shrinking navbar feature (slightly bugged & incomplete)
    - shrinking navbar incomplete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import { toggleDarkMode } from '../redux/settings';
 import Logo from '../assets/world-map-svgrepo-com.svg'
@@ -7,12 +8,32 @@ function Navbar() {
     const { darkMode } = useSelector(state => state.displayValue);
     const dispatch = useDispatch();
 
+    const [isMinimized, minimizeNavbar] = useState(false);
+
+    const handleScroll = () => {
+        if (window.scrollY > 300) {
+            minimizeNavbar(true);
+        } else {
+            minimizeNavbar(false);
+        }
+    };
+
+    // Add scroll event listener when the component mounts
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);// bugged delay transition, as dark mode delay is affecting navbar size change for light and dark mode - must separate into separate divs
+
     return (
         <>
-            <div className={`w-full h-20 fixed top-0 z-50 flex items-center ${darkMode ? 'bg-dark-blue-dark-mode-elements text-white' : 'bg-white text-very-dark-blue-light-mode-text'} px-4 sm:px-8 md:px-24 shadow-lg transition-all duration-500 ease-in-out`}>
+            <div className={`w-full  ${isMinimized ? 'h-14' : 'h-20'} fixed top-0 z-50 flex items-center ${darkMode ? 'bg-dark-blue-dark-mode-elements text-white duration-100' : 'bg-white text-very-dark-blue-light-mode-text duration-500'} px-4 sm:px-8 md:px-24 shadow-lg transition-all ease-in-out`}>
+                
                 <div className="w-full h-10 flex items-center justify-between">
+
                     <div className="w-48 sm:w-60 h-full flex items-center relative">
-                        <img src={Logo} alt="" className="h-12 sm:h-14 mr-4" />
+                        <img src={Logo} alt="" className={`${isMinimized ? 'h-10 sm:h-12' : 'h-12 sm:h-14'} mr-4 transition-all duration-300 ease-in-out`} />
                         <h1 className="text-2xl sm:text-3xl font-extrabold">
                             WorldView
                         </h1>
@@ -22,12 +43,15 @@ function Navbar() {
                             <i className="las la-external-link-alt ml-1"></i>
                         </a>
                     </div>
+
                     <button onClick={() => dispatch(toggleDarkMode())}
                         className="font-semibold px-3 py-2">
                         <i className="las la-moon mr-2 text-xl"></i>
                         <span className="hidden custom-xs:inline">Dark Mode</span>
                     </button>
+
                 </div>
+
             </div>
         </>
     );
