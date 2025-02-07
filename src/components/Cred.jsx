63cbb8d820e997e18fca4958bc471a835b738314
Bryan Li <76718507+Zy8712@users.noreply.github.com>
2025-02-06 21:01:34 -0500

    Added creds.
    Specifics:
    - added credits to Frontend Mentor for original design
    - added credits to myself for coding the website
    - altered styling slightly to accommodate for new credits div
    - added relative property to Home page component
    - added links in credits with styling

---
+++ src/components/Cred.jsx
@@ -0,0 +1,19 @@
+
+export default function Cred() {
+    return (
+        <>
+            <div className="absolute bottom-0 left-1/2 -translate-x-1/2 w-screen h-4 text-xs font-bold">
+                <p className="text-center">
+                    Developed/Coded by:&nbsp;
+                    <a href="https://github.com/Zy8712" target="_blank" className="text-blue-600 underline underline-offset-1">
+                        Bryan Li
+                    </a>.
+                    Original Design by:&nbsp;
+                    <a href="https://www.frontendmentor.io/" target="_blank" className="text-blue-600 underline underline-offset-1">
+                        Frontend Mentor
+                    </a>.
+                </p>
+            </div>
+        </>
+    );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import FilterAndSearchBar from "../components/FilterAndSearchBar";
 import RenderCards from "../components/RenderCards";
+import Cred from "../components/Cred";
 import { useSelector } from "react-redux";
 import "../App.css";
 
@@ -9,9 +10,10 @@ function Home() {
 
     return (
         <>
-            <div className={`w-full h-auto min-h-screen flex flex-col pt-32 pb-20 px-4 sm:px-10 lg:px-24 ${darkMode ? 'bg-very-dark-blue-dark-mode-background text-white' : 'bg-very-light-gray-light-mode-background text-very-dark-blue-light-mode-text transition-all duration-500 ease-in-out'}`}>
+            <div className={`relative w-full h-auto min-h-screen flex flex-col pt-32 pb-20 px-4 sm:px-10 lg:px-24 ${darkMode ? 'bg-very-dark-blue-dark-mode-background text-white' : 'bg-very-light-gray-light-mode-background text-very-dark-blue-light-mode-text transition-all duration-500 ease-in-out'}`}>
                 <FilterAndSearchBar />
                 <RenderCards />
+                <Cred />
             </div>
         </>
     );
