e317bdf8578fbdde3581aa44338693f3eb00bca2
Bryan Li <76718507+Zy8712@users.noreply.github.com>
2025-02-08 16:28:32 -0500

    Refactored navbar code. Improved shrinking.
    Specifics:
    - refactored navbar code to separate background color transition and navbar size change
    - separated background and size change into separate divs
    - added shrinking feature to navbar text and added changes to margins and positioning

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,30 +28,32 @@ function Navbar() {
 
     return (
         <>
-            <div className={`w-full  ${isMinimized ? 'h-14' : 'h-20'} fixed top-0 z-50 flex items-center ${darkMode ? 'bg-dark-blue-dark-mode-elements text-white duration-100' : 'bg-white text-very-dark-blue-light-mode-text duration-500'} px-4 sm:px-8 md:px-24 shadow-lg transition-all ease-in-out`}>
-                
-                <div className="w-full h-10 flex items-center justify-between">
+            <div className={`w-full ${isMinimized ? 'h-14' : 'h-20'} fixed top-0 z-50 flex items-center shadow-lg duration-300 transition-all ease-in-out`}>
+                <div className={`w-full min-h-full flex items-center px-4 sm:px-8 md:px-24  ${darkMode ? 'bg-dark-blue-dark-mode-elements text-white duration-100' : 'bg-white text-very-dark-blue-light-mode-text duration-500'} transition-all ease-in-out`}>
 
-                    <div className="w-48 sm:w-60 h-full flex items-center relative">
-                        <img src={Logo} alt="" className={`${isMinimized ? 'h-10 sm:h-12' : 'h-12 sm:h-14'} mr-4 transition-all duration-300 ease-in-out`} />
-                        <h1 className="text-2xl sm:text-3xl font-extrabold">
-                            WorldView
-                        </h1>
-                        <a href="https://restcountries.com/"
-                            className="w-64 text-[10px] sm:text-xs font-semibold underline underline-offset-2 absolute -bottom-2 sm:-bottom-3 -right-36">
-                            Powered by REST Countries API
-                            <i className="las la-external-link-alt ml-1"></i>
-                        </a>
+                    <div className="w-full h-10 flex items-center justify-between">
+
+                        <div className="w-48 sm:w-60 h-full flex items-center relative">
+                            <img src={Logo} alt="" className={`${isMinimized ? 'h-10 sm:h-12 mr-3' : 'h-12 sm:h-14 mr-4'} transition-all duration-300 ease-in-out`} />
+                            <h1 className={`${isMinimized ? 'text-xl sm:text-2xl' : 'text-2xl sm:text-3xl'} font-extrabold duration-300 transition-all ease-in-out`}>
+                                WorldView
+                            </h1>
+                            <a href="https://restcountries.com/"
+                                className={`w-64 font-semibold underline underline-offset-2 absolute ${isMinimized ? ' text-[8px] sm:text-[10px] -bottom-0 sm:-bottom-1 -right-28' : 'text-[10px] sm:text-xs -bottom-2 sm:-bottom-3 -right-36'} duration-300 transition-all ease-in-out`}>
+                                Powered by REST Countries API
+                                <i className="las la-external-link-alt ml-1"></i>
+                            </a>
+                        </div>
+
+                        <button onClick={() => dispatch(toggleDarkMode())}
+                            className="font-semibold px-3 py-2">
+                            <i className="las la-moon mr-2 text-xl"></i>
+                            <span className="hidden custom-xs:inline">Dark Mode</span>
+                        </button>
+
                     </div>
 
-                    <button onClick={() => dispatch(toggleDarkMode())}
-                        className="font-semibold px-3 py-2">
-                        <i className="las la-moon mr-2 text-xl"></i>
-                        <span className="hidden custom-xs:inline">Dark Mode</span>
-                    </button>
-
                 </div>
-
             </div>
         </>
     );
